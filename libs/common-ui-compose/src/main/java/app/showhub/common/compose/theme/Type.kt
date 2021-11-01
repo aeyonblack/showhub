4bcac1549a282f146a99c57e765caf7c77cb56f4
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-01 13:52:25 +0200

    added fonts

--- libs/common-ui-compose/src/main/java/app/showhub/common/compose/theme/Type.kt
+++ libs/common-ui-compose/src/main/java/app/showhub/common/compose/theme/Type.kt
@@ -1,2 +1,74 @@
 package app.showhub.common.compose.theme
 
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+import com.tanya.common.ui.compose.R
+
+private val fonts = FontFamily(
+    Font(R.font.rubik_regular),
+    Font(R.font.rubik_medium, FontWeight.W500),
+    Font(R.font.rubik_bold, FontWeight.Bold)
+)
+
+val typography = Typography(
+    h1 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold
+    ),
+    h2 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold
+    ),
+    h3 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold
+    ),
+    h4 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 40.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold
+    ),
+    h6 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.W500,
+        lineHeight = 28.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.W500,
+        lineHeight = 22.sp
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.W500
+    ),
+    body1 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 28.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 16.sp
+    ),
+    button = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold
+    ),
+    caption = TextStyle(
+        fontFamily = fonts
+    ),
+    overline = TextStyle(
+        letterSpacing = 0.08.em
+    )
+)
