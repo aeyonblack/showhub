1660d4b941aa4a8209102da93540b3b9f3567637
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-01 13:46:46 +0200

    added material theme shapes

--- libs/common-ui-compose/src/main/java/app/showhub/common/compose/theme/Shape.kt
+++ libs/common-ui-compose/src/main/java/app/showhub/common/compose/theme/Shape.kt
@@ -1,2 +1,16 @@
 package app.showhub.common.compose.theme
 
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val shapes = Shapes(
+    small = RoundedCornerShape(percent = 50),
+    medium = RoundedCornerShape(size = 0f),
+    large = RoundedCornerShape(
+        topStart = 16.dp,
+        topEnd = 0.dp,
+        bottomEnd = 0.dp,
+        bottomStart = 16.dp
+    )
+)
