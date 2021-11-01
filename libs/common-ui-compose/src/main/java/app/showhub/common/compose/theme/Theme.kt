38a90f657f9a03eaea40d7d5d0fd6d84e1018bdd
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-01 17:31:53 +0200

    added gradient scrim modifier extension

--- libs/common-ui-compose/src/main/java/app/showhub/common/compose/theme/Theme.kt
+++ libs/common-ui-compose/src/main/java/app/showhub/common/compose/theme/Theme.kt
@@ -25,7 +25,7 @@ private val showhubDefault = darkColors(
 fun ShowhubTheme(content: @Composable () -> Unit) {
     val systemUiController = rememberSystemUiController()
     systemUiController.setSystemBarsColor(
-        color = showhubDefault.background,
+        color = Color.Transparent,
         darkIcons = false
     )
     val elevations = Elevations(card = 1.dp)
