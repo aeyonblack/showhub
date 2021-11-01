8b5461de3ee045fdf68f36a2c5209e382477aa61
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-01 15:08:36 +0200

    updated the app theme

--- app/src/main/java/com/tanya/showhub/MainActivity.kt
+++ app/src/main/java/com/tanya/showhub/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.tanya.showhub.ui.theme.ShowhubTheme
+import app.showhub.common.compose.theme.ShowhubTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- libs/common-ui-compose/src/main/java/app/showhub/common/compose/theme/Theme.kt
+++ libs/common-ui-compose/src/main/java/app/showhub/common/compose/theme/Theme.kt
@@ -1,2 +1,34 @@
 package app.showhub.common.compose.theme
 
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+private val showhubDefault = darkColors(
+    primary = pink500,
+    primaryVariant = pink600,
+    onPrimary = Color.White,
+    secondary = green400,
+    secondaryVariant = green500,
+    onSecondary = Color.Black,
+    background = Color.Black,
+    onBackground = Color.White,
+    surface = gray800,
+    onSurface = Color.White
+)
+
+@Composable
+fun ShowhubTheme(content: @Composable () -> Unit) {
+    val elevations = Elevations(card = 1.dp)
+    CompositionLocalProvider(LocalElevations provides elevations) {
+        MaterialTheme(
+            colors = showhubDefault,
+            typography = typography,
+            shapes = shapes,
+            content = content
+        )
+    }
+}
