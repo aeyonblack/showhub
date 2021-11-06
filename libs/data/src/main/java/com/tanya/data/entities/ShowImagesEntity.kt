ad38deffe9c2e9275a156eded20f5c2a4d7c787d
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-06 21:59:53 +0200

    added and show images entity

---
+++ libs/data/src/main/java/com/tanya/data/entities/ShowImagesEntity.kt
@@ -0,0 +1,32 @@
+package com.tanya.data.entities
+
+import androidx.room.*
+
+/**
+ * Contains show images from TMDB
+ */
+@Entity(
+    tableName = "show_images",
+    indices = [
+        Index(value = ["show_id"])
+    ],
+    foreignKeys = [
+        ForeignKey(
+            entity = ShowEntity::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("show_id"),
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+data class ShowImagesEntity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id") override val id: Long = 0,
+    @ColumnInfo(name = "showId") val showId: Long,
+    @ColumnInfo(name = "path") override val path: String,
+    @ColumnInfo(name = "type") override val type: ImageType,
+    @ColumnInfo(name = "lang") override val language: String?,
+    @ColumnInfo(name = "rating") override val rating: Float = 0f,
+    @ColumnInfo(name = "is_primary") override val isPrimary: Boolean = false
+): BaseEntity, TmdbImageEntity
