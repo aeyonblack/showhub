527ad365b0a7226a6a995c9854a4e5dbb54854fc
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-06 22:03:07 +0200

    added a ShowImages util class

---
+++ libs/data/src/main/java/com/tanya/data/entities/ShowImages.kt
@@ -0,0 +1,10 @@
+package com.tanya.data.entities
+
+/**
+ * Util for getting the highest rated show images to display
+ */
+data class ShowImages(val images: ShowImagesEntity) {
+
+    val backdrop by lazy {  }
+
+}
