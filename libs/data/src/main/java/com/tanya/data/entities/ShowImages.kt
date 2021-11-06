438e07f271ccb96c62c1dcc5dbbf29586c3e8afb
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-06 22:07:26 +0200

    find highest rated show image for specific type

--- libs/data/src/main/java/com/tanya/data/entities/ShowImages.kt
+++ libs/data/src/main/java/com/tanya/data/entities/ShowImages.kt
@@ -1,10 +1,23 @@
 package com.tanya.data.entities
 
+import com.tanya.data.entities.ImageType.*
+
 /**
  * Util for getting the highest rated show images to display
  */
-data class ShowImages(val images: ShowImagesEntity) {
+data class ShowImages(val images: List<ShowImagesEntity>) {
 
-    val backdrop by lazy {  }
+    val backdrop by lazy(LazyThreadSafetyMode.NONE) {
+        findHighestRatedForType(BACKDROP)
+    }
+
+    val poster by lazy(LazyThreadSafetyMode.NONE) {
+        findHighestRatedForType(POSTER)
+    }
+
+    private fun findHighestRatedForType(type: ImageType): ShowImagesEntity? {
+        return images.filter { it.type == type }
+            .maxByOrNull { it.rating + (if (it.isPrimary) 10f else 0f) }
+    }
 
 }
