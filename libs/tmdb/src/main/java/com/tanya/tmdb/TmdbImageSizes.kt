b7ea7aab45c60e77c661f8276ee5c179853aa4f9
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-06 15:55:56 +0200

    added image sizes for poster and other images

--- libs/tmdb/src/main/java/com/tanya/tmdb/TmdbImageSizes.kt
+++ libs/tmdb/src/main/java/com/tanya/tmdb/TmdbImageSizes.kt
@@ -1,4 +1,38 @@
 package com.tanya.tmdb
 
+/**
+ * Holds sizes for different image types as specified by tmdb
+ */
 object TmdbImageSizes {
+
+    // base url for getting poster and other images
+    const val baseImageUrl = "https://image.tmdb.org/t/p/"
+
+    val backdropSizes = listOf(
+        "w300",
+        "w780",
+        "w1280",
+        "original"
+    )
+
+    val posterSizes = listOf(
+        "w92",
+        "w154",
+        "w185",
+        "w342",
+        "w500",
+        "w780",
+        "original"
+    )
+
+    val logoSizes = listOf(
+        "w45",
+        "w92",
+        "w154",
+        "w185",
+        "w300",
+        "w500",
+        "original"
+    )
+
 }
