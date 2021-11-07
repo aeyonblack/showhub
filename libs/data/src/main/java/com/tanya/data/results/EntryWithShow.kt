4722a4d3a9adef09f7781c2ffb7d1424d247d04a
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-07 08:36:07 +0200

    added interface for different show entries

--- libs/data/src/main/java/com/tanya/data/results/EntryWithShow.kt
+++ libs/data/src/main/java/com/tanya/data/results/EntryWithShow.kt
@@ -1,4 +1,22 @@
 package com.tanya.data.results
 
-interface EntryWithShow {
+import com.tanya.data.Entry
+import com.tanya.data.entities.ShowEntity
+import com.tanya.data.entities.ShowImagesEntity
+import java.util.*
+
+interface EntryWithShow<ET : Entry> {
+    var entry: ET
+    var relations: List<ShowEntity>
+    var images: List<ShowImagesEntity>
+
+    val show: ShowEntity
+        get() {
+            check(relations.size == 1)
+            return relations[0]
+        }
+
+    val poster: ShowImagesEntity?
+
+    fun generateStableId() = Objects.hash(entry::class.java.name, entry.showId).toLong()
 }
