842a759f7d133b9fd0e393d64646e48bf1a3dd0d
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-07 15:21:26 +0200

    added show data source interface

--- libs/data/src/main/java/com/tanya/data/repositories/shows/ShowDataSource.kt
+++ libs/data/src/main/java/com/tanya/data/repositories/shows/ShowDataSource.kt
@@ -1,4 +1,7 @@
 package com.tanya.data.repositories.shows
 
+import com.tanya.data.entities.ShowEntity
+
 interface ShowDataSource {
+    suspend fun getShow(show: ShowEntity): Result<ShowEntity>
 }
