44416cb54f7e62792784920ea824ab77ff05b5d5
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-07 10:21:18 +0200

    added abstract methods entity dao

--- libs/data/src/main/java/com/tanya/data/daos/EntityDao.kt
+++ libs/data/src/main/java/com/tanya/data/daos/EntityDao.kt
@@ -1,2 +1,41 @@
 package com.tanya.data.daos
 
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Transaction
+import androidx.room.Update
+import com.tanya.data.entities.BaseEntity
+
+abstract class EntityDao<in E : BaseEntity> {
+
+    @Insert
+    abstract suspend fun insert(entity: E): Long
+
+    @Insert
+    abstract suspend fun insertAll(vararg entity: E)
+
+    @Insert
+    abstract suspend fun insertAll(entities: List<E>)
+
+    @Update
+    abstract suspend fun update(entity: E)
+
+    @Delete
+    abstract suspend fun delete(entity: E): Int
+
+    @Transaction
+    open suspend fun withTransaction(tx: suspend () -> Unit) = tx()
+
+    @Transaction
+    open suspend fun insertOrUpdate(entities: List<E>) {
+        entities.forEach {
+            insertOrUpdate(it)
+        }
+    }
+
+    suspend fun insertOrUpdate(entity: E): Long =
+        if (entity.id == 0L) insert(entity) else {
+            update(entity)
+            entity.id
+        }
+}
