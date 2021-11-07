cd02ad8494dd2519cc13d81b1a9fde6bd9b3e379
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-07 15:19:49 +0200

    added result wrapper class

--- libs/base/src/main/java/com/tanya/base/data/entities/Result.kt
+++ libs/base/src/main/java/com/tanya/base/data/entities/Result.kt
@@ -1,5 +1,16 @@
 package com.tanya.base.data.entities
 
 sealed class Result<T> {
+    open fun get(): T? = null
 
+    fun getOrThrow(): T = when(this) {
+        is Success -> get()
+        is ErrorResult -> throw throwable
+    }
 }
+
+data class Success<T>(val data: T, val responseModified: Boolean = true) : Result<T>() {
+    override fun get(): T = data
+}
+
+data class ErrorResult<T>(val throwable: Throwable) : Result<T>()
