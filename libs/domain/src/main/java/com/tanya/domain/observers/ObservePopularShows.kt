ac70ee3c2270f48e6f2c6ebc12073e6b0accc4fd
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-09 21:49:50 +0200

    added popular shows observer

---
+++ libs/domain/src/main/java/com/tanya/domain/observers/ObservePopularShows.kt
@@ -0,0 +1,19 @@
+package com.tanya.domain.observers
+
+import com.tanya.data.daos.PopularDao
+import com.tanya.data.results.PopularEntryWithShow
+import com.tanya.domain.SubjectInteractor
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ObservePopularShows @Inject constructor(
+    private val popularShowsRepository: PopularDao
+) : SubjectInteractor<ObservePopularShows.Params, List<PopularEntryWithShow>>() {
+
+    override fun createObservable(
+        params: ObservePopularShows.Params
+    ): Flow<List<PopularEntryWithShow>> =
+        popularShowsRepository.entriesObservable(params.count, 0)
+
+    data class Params(val count: Int  = 20)
+}
