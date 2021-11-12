8db46cca69653cece13aea9a9aeb55c10158d4b2
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-12 13:42:51 +0200

    setup showhub room database

--- libs/data-android/src/main/java/com/tanya/data/android/repository/database/ShowhubRoomDatabase.kt
+++ libs/data-android/src/main/java/com/tanya/data/android/repository/database/ShowhubRoomDatabase.kt
@@ -1,6 +1,23 @@
 package com.tanya.data.android.repository.database
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.tanya.data.ShowhubDatabase
+import com.tanya.data.entities.PopularShowEntity
+import com.tanya.data.entities.ShowEntity
+import com.tanya.data.entities.ShowImagesEntity
+import com.tanya.data.entities.TrendingShowEntity
 
-abstract class ShowhubRoomDatabase: RoomDatabase() {
+@Database(
+    entities = [
+        ShowEntity::class,
+        ShowImagesEntity::class,
+        PopularShowEntity::class,
+        TrendingShowEntity::class
+    ],
+    version = 1
+)
+@TypeConverters(TypeConverters::class)
+abstract class ShowhubRoomDatabase: RoomDatabase(), ShowhubDatabase {
 }
