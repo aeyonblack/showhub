d592a84a95bac77107ec2e7d7a5c23138a24e8fa
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-12 17:51:34 +0200

    setting up the main home screen prototype

--- features/ui-home/src/main/java/com/tanya/ui/home/Home.kt
+++ features/ui-home/src/main/java/com/tanya/ui/home/Home.kt
@@ -1,14 +1,20 @@
 package com.tanya.ui.home
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import app.showhub.common.compose.components.AppBar
 import app.showhub.common.compose.utils.rememberFlowWithLifeCycle
+import com.tanya.data.results.EntryWithShow
 
 @Composable
 fun Home() {
@@ -36,5 +42,62 @@ internal fun Home(
         topBar = { AppBar("Hello!")},
         modifier = Modifier.fillMaxSize()
     ) {
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            item {
+                Spacer(modifier = Modifier.height(40.dp))
+            }
+            /*trending items*/
+            item {
+
+            }
+            /*popular items*/
+            item {
+
+            }
+        }
     }
+}
+
+@Composable
+private fun <T: EntryWithShow<*>> CarouselWithHeader(
+    items: List<T>,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier) {
+
+    }
+}
+
+@Composable
+private fun Header(
+    title: String,
+    modifier: Modifier = Modifier,
+    content: @Composable RowScope.() -> Unit = {}
+) {
+    Row(modifier) {
+        Spacer(modifier = Modifier.width(32.dp))
+        
+        Text(
+            text = title,
+            style = MaterialTheme.typography.h4,
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .padding(8.dp)
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        content()
+    }
+}
+
+@Composable
+private fun <T: EntryWithShow<*>> EntryShowCarousel(
+    items: List<T>,
+    modifier: Modifier = Modifier
+) {
+
 }
