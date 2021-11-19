e8d2e5418ee5cefe39edcfbee2bb8065235f1710
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-19 19:05:27 +0200

    added show details composable screen

--- features/ui-showdetails/src/main/java/com/tanya/ui/showdetails/ShowDetails.kt
+++ features/ui-showdetails/src/main/java/com/tanya/ui/showdetails/ShowDetails.kt
@@ -1,2 +1,15 @@
 package com.tanya.ui.showdetails
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ShowDetails() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(text = "ShowDetails")
+    }
+}
