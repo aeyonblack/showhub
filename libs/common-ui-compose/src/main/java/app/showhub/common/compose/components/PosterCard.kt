48c92b1b823468c6951b44ed58279c6259953549
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-19 16:30:51 +0200

    clean up

--- libs/common-ui-compose/src/main/java/app/showhub/common/compose/components/PosterCard.kt
+++ libs/common-ui-compose/src/main/java/app/showhub/common/compose/components/PosterCard.kt
@@ -1,6 +1,5 @@
 package app.showhub.common.compose.components
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -52,8 +51,6 @@ fun PosterCard(
                         modifier = Modifier.matchParentSize()
                     )
                 }
-            } else {
-                Log.d("posterCard", "poster is null")
             }
         }
     }
