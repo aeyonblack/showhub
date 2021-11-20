d8734c347a9a968fa72eb01a6ed6c1a0ffacc461
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-20 21:38:21 +0200

    mock ui for show details

--- features/ui-showdetails/src/main/java/com/tanya/ui/showdetails/ShowDetails.kt
+++ features/ui-showdetails/src/main/java/com/tanya/ui/showdetails/ShowDetails.kt
@@ -1,15 +1,22 @@
 package com.tanya.ui.showdetails
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
+import app.showhub.common.compose.components.AppBar
 import app.showhub.common.compose.utils.rememberFlowWithLifeCycle
+import com.google.accompanist.insets.statusBarsPadding
+import com.tanya.common.ui.resources.R
 
 @Composable
 fun ShowDetails() {
@@ -33,7 +40,51 @@ internal fun ShowDetails(
 internal fun ShowDetails(
     state: ShowDetailsViewState
 ) {
-    Column(modifier = Modifier.padding(16.dp)) {
-        state.show.title?.let { Text(text = it) }
+    ConstraintLayout() {
+        val (topAppBar, content) = createRefs()
+        AppBar(
+            title = state.show.title ?: "",
+            modifier = Modifier
+                .fillMaxWidth()
+                .statusBarsPadding()
+                .constrainAs(topAppBar) {
+                    top.linkTo(parent.top)
+                }
+        )
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .constrainAs(content) {
+                    top.linkTo(parent.top)
+                }
+        ) {
+            repeat(50) {
+                item {
+                    Text(
+                        text = "List Content",
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+internal fun AppBar(
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .fillMaxWidth(0.6f)
+                .padding(16.dp)
+        )
     }
 }
