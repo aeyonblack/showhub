e3d760da730d91e97f0696166d1bf12ed783118c
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-23 19:26:03 +0200

    defined a navigate up action in show details actions

--- features/ui-showdetails/src/main/java/com/tanya/ui/showdetails/ShowDetailsAction.kt
+++ features/ui-showdetails/src/main/java/com/tanya/ui/showdetails/ShowDetailsAction.kt
@@ -1,5 +1,6 @@
 package com.tanya.ui.showdetails
 
 internal sealed class ShowDetailsAction {
+    object NavigateUp : ShowDetailsAction()
     data class OpenShowDetails(val showId: Long): ShowDetailsAction()
 }
