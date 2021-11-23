c199e0359942067f975ace24bafbc08f5b9a2d9e
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-23 14:47:22 +0200

    added related shows to state and update viewmodel

--- features/ui-showdetails/src/main/java/com/tanya/ui/showdetails/ShowDetailsViewModel.kt
+++ features/ui-showdetails/src/main/java/com/tanya/ui/showdetails/ShowDetailsViewModel.kt
@@ -8,8 +8,10 @@ import com.tanya.base.base.InvokeStarted
 import com.tanya.base.base.InvokeStatus
 import com.tanya.base.base.InvokeSuccess
 import com.tanya.common.ui.view.util.ObservableLoadingCounter
+import com.tanya.domain.interactors.UpdateRelatedShows
 import com.tanya.domain.interactors.UpdateShowDetails
 import com.tanya.domain.interactors.UpdateShowImages
+import com.tanya.domain.observers.ObserveRelatedShows
 import com.tanya.domain.observers.ObserveShowDetails
 import com.tanya.domain.observers.ObserveShowImages
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -23,7 +25,9 @@ internal class ShowDetailsViewModel @Inject constructor(
     private val updateShowDetails: UpdateShowDetails,
     observeShowDetails: ObserveShowDetails,
     private val updateShowImages: UpdateShowImages,
-    observeShowImages: ObserveShowImages
+    observeShowImages: ObserveShowImages,
+    private val updateRelatedShows: UpdateRelatedShows,
+    observeRelatedShows: ObserveRelatedShows
 ) : ViewModel() {
     private val showId: Long = savedStateHandle.get("showId")!!
     private val loadingState = ObservableLoadingCounter()
@@ -31,11 +35,13 @@ internal class ShowDetailsViewModel @Inject constructor(
     val state = combine(
         loadingState.observable,
         observeShowDetails.flow,
-        observeShowImages.flow
-    ) { refreshing, show, showImages ->
+        observeShowImages.flow,
+        observeRelatedShows.flow
+    ) { refreshing, show, showImages, relatedShows ->
         ShowDetailsViewState(
             show = show,
             backdropImage = showImages.backdrop,
+            relatedShows = relatedShows,
             refreshing = refreshing
         )
     }.stateIn(
@@ -47,13 +53,15 @@ internal class ShowDetailsViewModel @Inject constructor(
     init {
         observeShowDetails(ObserveShowDetails.Params(showId))
         observeShowImages(ObserveShowImages.Params(showId))
+        observeRelatedShows(ObserveRelatedShows.Params(showId))
 
-        refresh(true)
+        refresh()
     }
 
-    private fun refresh(forceLoad: Boolean) {
+    private fun refresh(forceLoad: Boolean = true) {
         updateShowDetails(UpdateShowDetails.Params(showId, forceLoad)).watchStatus()
         updateShowImages(UpdateShowImages.Params(showId, forceLoad)).watchStatus()
+        updateRelatedShows(UpdateRelatedShows.Params(showId, forceLoad)).watchStatus()
     }
 
     private fun Flow<InvokeStatus>.watchStatus() = viewModelScope.launch { collectStatus() }
--- features/ui-showdetails/src/main/java/com/tanya/ui/showdetails/ShowDetailsViewState.kt
+++ features/ui-showdetails/src/main/java/com/tanya/ui/showdetails/ShowDetailsViewState.kt
@@ -3,11 +3,13 @@ package com.tanya.ui.showdetails
 import androidx.compose.runtime.Immutable
 import com.tanya.data.entities.ShowEntity
 import com.tanya.data.entities.ShowImagesEntity
+import com.tanya.data.results.RelatedShowEntryWithShow
 
 @Immutable
 internal data class ShowDetailsViewState(
     val show: ShowEntity = ShowEntity.EMPTY_SHOW,
     val backdropImage: ShowImagesEntity? = null,
+    val relatedShows: List<RelatedShowEntryWithShow> = emptyList(),
     val refreshing: Boolean = false
 ) {
     companion object {
