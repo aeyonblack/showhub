3333aaa1b813676c232420373944e973083bb33b
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-24 18:49:26 +0200

    added trakt episode data source

--- libs/data/src/main/java/com/tanya/data/repositories/episodes/TraktEpisodeDataSource.kt
+++ libs/data/src/main/java/com/tanya/data/repositories/episodes/TraktEpisodeDataSource.kt
@@ -1,21 +1,33 @@
 package com.tanya.data.repositories.episodes
 
+import com.tanya.base.data.entities.ErrorResult
 import com.tanya.base.data.entities.Result
+import com.tanya.base.extensions.executeWithRetry
+import com.tanya.base.extensions.toResult
 import com.tanya.data.entities.EpisodeEntity
 import com.tanya.data.mappers.ShowIdToTraktId
+import com.tanya.data.mappers.TraktEpisodeToEpisodeEntity
+import com.uwetrottmann.trakt5.enums.Extended
 import com.uwetrottmann.trakt5.services.Episodes
 import javax.inject.Inject
 import javax.inject.Provider
 
 class TraktEpisodeDataSource @Inject constructor(
     private val traktIdMapper: ShowIdToTraktId,
-    private val service: Provider<Episodes>
+    private val service: Provider<Episodes>,
+    private val mapper: TraktEpisodeToEpisodeEntity
 ): EpisodeDataSource {
     override suspend fun getEpisode(
         showId: Long,
         seasonNumber: Int,
         episodeNumber: Int
     ): Result<EpisodeEntity> {
-        TODO("Not yet implemented")
+        val traktId = traktIdMapper.map(showId)
+            ?: return ErrorResult(IllegalArgumentException("No trakt id for show with id = $showId"))
+        return service
+            .get()
+            .summary(traktId.toString(), seasonNumber, episodeNumber, Extended.FULL)
+            .executeWithRetry()
+            .toResult(mapper::map)
     }
 }
