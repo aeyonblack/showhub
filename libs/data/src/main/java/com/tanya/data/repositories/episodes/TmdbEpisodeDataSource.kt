6eae584e0ac766051015d3fa77205a47a30f8a64
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-24 18:32:51 +0200

    added tmdb episode data source

--- libs/data/src/main/java/com/tanya/data/repositories/episodes/TmdbEpisodeDataSource.kt
+++ libs/data/src/main/java/com/tanya/data/repositories/episodes/TmdbEpisodeDataSource.kt
@@ -1,17 +1,24 @@
 package com.tanya.data.repositories.episodes
 
-import com.tanya.base.data.entities.Result
-import com.tanya.data.entities.EpisodeEntity
+import com.tanya.base.extensions.executeWithRetry
+import com.tanya.base.extensions.toResult
+import com.tanya.data.mappers.ShowIdToTmdbIdMapper
+import com.tanya.data.mappers.TmdbEpisodeToEpisodeEntity
+import com.uwetrottmann.tmdb2.Tmdb
 import javax.inject.Inject
 
 class TmdbEpisodeDataSource @Inject constructor(
-
+    private val tmdbIdMapper: ShowIdToTmdbIdMapper,
+    private val tmdb: Tmdb,
+    private val mapper: TmdbEpisodeToEpisodeEntity
 ): EpisodeDataSource {
     override suspend fun getEpisode(
         showId: Long,
         seasonNumber: Int,
         episodeNumber: Int
-    ): Result<EpisodeEntity> {
-        TODO("Not yet implemented")
-    }
+    ) = tmdb
+        .tvEpisodesService()
+        .episode(tmdbIdMapper.map(showId), seasonNumber, episodeNumber, null)
+        .executeWithRetry()
+        .toResult(mapper::map)
 }
