1fb4baa3dbddaed17af38f9d0852324cbe9fd748
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-25 19:56:12 +0200

    added a dagger module for followed shows

--- libs/data-android/src/main/java/com/tanya/data/android/repository/library/FollowedShowsModule.kt
+++ libs/data-android/src/main/java/com/tanya/data/android/repository/library/FollowedShowsModule.kt
@@ -1,7 +1,15 @@
 package com.tanya.data.android.repository.library
 
+import com.tanya.data.repositories.followedshows.FollowedShowsDataSource
+import com.tanya.data.repositories.followedshows.TraktFollowedShowsDataSource
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class FollowedShowsModule {
+    @Binds
+    abstract fun bind(source: TraktFollowedShowsDataSource): FollowedShowsDataSource
+}
