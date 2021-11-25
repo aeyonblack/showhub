f875929b80ac89089be598881c4e711cec412d26
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-25 18:35:17 +0200

    observe episode watches

--- libs/data/src/main/java/com/tanya/data/repositories/episodes/EpisodeWatchStore.kt
+++ libs/data/src/main/java/com/tanya/data/repositories/episodes/EpisodeWatchStore.kt
@@ -2,7 +2,9 @@ package com.tanya.data.repositories.episodes
 
 import com.tanya.data.DatabaseTransactionRunner
 import com.tanya.data.daos.EpisodeWatchEntityDao
+import com.tanya.data.entities.EpisodeWatchEntity
 import com.tanya.data.syncers.syncerForEntity
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class EpisodeWatchStore @Inject constructor(
@@ -16,5 +18,7 @@ class EpisodeWatchStore @Inject constructor(
         {entity, id -> entity.copy(id = id ?: 0)}
     )
 
+    fun observeEpisodeWatches(episodeId: Long): Flow<List<EpisodeWatchEntity>> =
+        dao.watchesForEpisodeObservable(episodeId)
 
 }
