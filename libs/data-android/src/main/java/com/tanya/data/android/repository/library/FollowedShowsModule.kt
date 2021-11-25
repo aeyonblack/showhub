8d63092bc92358ba7c0c45d8c39738ffa169b935
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-25 19:03:31 +0200

    added followed data source interface

---
+++ libs/data-android/src/main/java/com/tanya/data/android/repository/library/FollowedShowsModule.kt
@@ -0,0 +1,7 @@
+package com.tanya.data.android.repository.library
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
---
+++ libs/data/src/main/java/com/tanya/data/repositories/followedshows/FollowedShowsDataSource.kt
@@ -0,0 +1,11 @@
+package com.tanya.data.repositories.followedshows
+
+import com.tanya.data.entities.FollowedShowEntity
+import com.tanya.data.entities.ShowEntity
+
+interface FollowedShowsDataSource {
+    suspend fun getListShows(listId: Int): Result<List<Pair<FollowedShowEntity, ShowEntity>>>
+    suspend fun addShowIdsToList(listId: Int, shows: List<ShowEntity>): Result<Unit>
+    suspend fun removeShowIdsFromList(listId: Int, shows: List<ShowEntity>): Result<Unit>
+    suspend fun getFollowedListId(): Result<Int>
+}
