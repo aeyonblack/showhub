4ce85a4abc957a750a384b4edd2a18069564dfb7
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-26 12:28:51 +0200

    observe show follow status

---
+++ libs/domain/src/main/java/com/tanya/domain/observers/ObserveShowFollowStatus.kt
@@ -0,0 +1,17 @@
+package com.tanya.domain.observers
+
+import com.tanya.data.repositories.followedshows.FollowedShowsRepository
+import com.tanya.domain.SubjectInteractor
+import com.tanya.domain.observers.ObserveShowFollowStatus.Params
+import javax.inject.Inject
+
+class ObserveShowFollowStatus @Inject constructor(
+    private val repository: FollowedShowsRepository
+) : SubjectInteractor<Params, Boolean>() {
+
+    override fun createObservable(params: Params) =
+        repository.observeIsShowFollowed(params
+            .showId)
+
+    data class Params(val showId: Long)
+}
