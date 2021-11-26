caec89e5da8d6877a691feeeba27ba0f3883a049
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-26 11:36:57 +0200

    change season watch status

---
+++ libs/domain/src/main/java/com/tanya/domain/interactors/ChangeSeasonWatchedStatus.kt
@@ -0,0 +1,39 @@
+package com.tanya.domain.interactors
+
+import com.tanya.data.entities.ActionDate
+import com.tanya.data.repositories.episodes.SeasonsEpisodesRepository
+import com.tanya.domain.Interactor
+import com.tanya.domain.interactors.ChangeSeasonWatchedStatus.Action.UNWATCH
+import com.tanya.domain.interactors.ChangeSeasonWatchedStatus.Action.WATCHED
+import com.tanya.domain.interactors.ChangeSeasonWatchedStatus.Params
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class ChangeSeasonWatchedStatus @Inject constructor(
+    private val repository: SeasonsEpisodesRepository
+): Interactor<Params>() {
+
+    override suspend fun doWork(params: Params) = withContext(Dispatchers.IO) {
+        when (params.action) {
+            WATCHED -> repository.markSeasonWatched(
+                seasonId = params.seasonId,
+                onlyAired = params.onlyAired,
+                date = params.actionDate
+            )
+            UNWATCH -> repository.markSeasonUnwatched(params.seasonId)
+        }
+    }
+
+    data class Params(
+        val seasonId: Long,
+        val action: Action,
+        val onlyAired: Boolean = true,
+        val actionDate: ActionDate = ActionDate.NOW
+    )
+
+    enum class Action {
+        WATCHED,
+        UNWATCH
+    }
+}
