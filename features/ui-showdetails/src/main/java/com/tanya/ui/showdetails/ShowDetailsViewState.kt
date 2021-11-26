64d6ca0703df5c02ff9b3d1a89060c80936b02ce
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-26 15:16:04 +0200

    update show details with additional state values

--- features/ui-showdetails/src/main/java/com/tanya/ui/showdetails/ShowDetailsViewState.kt
+++ features/ui-showdetails/src/main/java/com/tanya/ui/showdetails/ShowDetailsViewState.kt
@@ -3,13 +3,20 @@ package com.tanya.ui.showdetails
 import androidx.compose.runtime.Immutable
 import com.tanya.data.entities.ShowEntity
 import com.tanya.data.entities.ShowImagesEntity
+import com.tanya.data.results.EpisodeWithSeason
 import com.tanya.data.results.RelatedShowEntryWithShow
+import com.tanya.data.results.SeasonWithEpisodesAndWatches
+import com.tanya.data.views.FollowedShowsWatchStats
 
 @Immutable
 internal data class ShowDetailsViewState(
+    val isFollowed: Boolean = false,
     val show: ShowEntity = ShowEntity.EMPTY_SHOW,
     val backdropImage: ShowImagesEntity? = null,
     val relatedShows: List<RelatedShowEntryWithShow> = emptyList(),
+    val seasons: List<SeasonWithEpisodesAndWatches> = emptyList(),
+    val nextEpisodeToWatch: EpisodeWithSeason? = null,
+    val watchStats: FollowedShowsWatchStats? = null,
     val refreshing: Boolean = false
 ) {
     companion object {
