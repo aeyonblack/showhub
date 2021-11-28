fe07a273e038c9637ad686e72ca5f2ce264fe75a
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-28 13:19:10 +0200

    setup worker factory and app initializers

--- app/src/main/java/com/tanya/showhub/ShowhubApplication.kt
+++ app/src/main/java/com/tanya/showhub/ShowhubApplication.kt
@@ -1,8 +1,25 @@
 package com.tanya.showhub
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import com.tanya.showhub.initializers.AppInitializers
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class ShowhubApplication: Application() {
+class ShowhubApplication: Application(), Configuration.Provider {
+
+    @Inject lateinit var initializers: AppInitializers
+    @Inject lateinit var workerFactory: HiltWorkerFactory
+
+    override fun onCreate() {
+        super.onCreate()
+        initializers.init(this)
+    }
+
+    override fun getWorkManagerConfiguration() =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
---
+++ app/src/main/java/com/tanya/showhub/initializers/AppInitializers.kt
@@ -0,0 +1,15 @@
+package com.tanya.showhub.initializers
+
+import android.app.Application
+import com.tanya.base.android.appinitializer.AppInitializer
+import javax.inject.Inject
+
+class AppInitializers @Inject constructor(
+    private val initializers: Set<@JvmSuppressWildcards AppInitializer>
+) {
+    fun init(application: Application) {
+        initializers.forEach {
+            it.init(application)
+        }
+    }
+}
