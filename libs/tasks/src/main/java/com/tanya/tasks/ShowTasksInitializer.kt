15329b88bcdb4695a7cd934025cfaec8066cfc19
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-28 13:04:57 +0200

    created show tasks initializer

--- libs/tasks/src/main/java/com/tanya/tasks/ShowTasksInitializer.kt
+++ libs/tasks/src/main/java/com/tanya/tasks/ShowTasksInitializer.kt
@@ -1,4 +1,15 @@
 package com.tanya.tasks
 
-class ShowTasksInitializer {
+import android.app.Application
+import com.tanya.base.actions.ShowTasks
+import com.tanya.base.android.appinitializer.AppInitializer
+import dagger.Lazy
+import javax.inject.Inject
+
+class ShowTasksInitializer @Inject constructor(
+    private val showTasks: Lazy<ShowTasks>
+) : AppInitializer {
+    override fun init(application: Application) {
+        showTasks.get().setupNightSyncs()
+    }
 }
