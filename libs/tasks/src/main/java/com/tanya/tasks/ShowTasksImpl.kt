8e5bf64954f69dc7be35f55da992c8cf751052e0
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-28 13:00:47 +0200

    added nightly syncs and idle followed show syncs to workmanager

--- libs/tasks/src/main/java/com/tanya/tasks/ShowTasksImpl.kt
+++ libs/tasks/src/main/java/com/tanya/tasks/ShowTasksImpl.kt
@@ -1,8 +1,8 @@
 package com.tanya.tasks
 
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
+import androidx.work.*
 import com.tanya.base.actions.ShowTasks
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class ShowTasksImpl @Inject constructor(
@@ -18,15 +18,44 @@ class ShowTasksImpl @Inject constructor(
     }
 
     override fun syncFollowedShows() {
-        TODO("Not yet implemented")
+        val request = OneTimeWorkRequest.Builder(SyncAllFollowedShows::class.java)
+            .addTag(SyncAllFollowedShows.TAG)
+            .build()
+        workManager.enqueue(request)
     }
 
     override fun syncFollowedShowsWhenIdle() {
-        TODO("Not yet implemented")
+        val request = OneTimeWorkRequest.Builder(SyncAllFollowedShows::class.java)
+            .addTag(SyncAllFollowedShows.TAG)
+            .setConstraints(
+                Constraints.Builder()
+                    .setRequiresDeviceIdle(true)
+                    .build()
+            )
+            .build()
+        workManager.enqueue(request)
     }
 
     override fun setupNightSyncs() {
-        TODO("Not yet implemented")
+        val request = PeriodicWorkRequest.Builder(
+            SyncAllFollowedShows::class.java,
+            24L,
+            TimeUnit.HOURS,
+            12L,
+            TimeUnit.HOURS
+        ).setConstraints(
+            Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .setRequiresBatteryNotLow(true)
+                .setRequiresDeviceIdle(true)
+                .build()
+        ).build()
+
+        workManager.enqueueUniquePeriodicWork(
+            SyncAllFollowedShows.NIGHTLY_SYNC_TAG,
+            ExistingPeriodicWorkPolicy.REPLACE,
+            request
+        )
     }
 
 }
