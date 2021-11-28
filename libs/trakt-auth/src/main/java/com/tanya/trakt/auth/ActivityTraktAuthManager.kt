9907b860ef6886144b2e92ad80b3ff0514f998f6
aeyonblack <tanyamasvitaog@gmail.com>
2021-11-28 14:22:10 +0200

    update trakt auth manager

---
+++ libs/trakt-auth/src/main/java/com/tanya/trakt/auth/ActivityTraktAuthManager.kt
@@ -0,0 +1,44 @@
+package com.tanya.trakt.auth
+
+import android.content.Context
+import android.content.Intent
+import dagger.Lazy
+import dagger.hilt.android.qualifiers.ApplicationContext
+import net.openid.appauth.AuthState
+import net.openid.appauth.AuthorizationRequest
+import net.openid.appauth.AuthorizationService
+import net.openid.appauth.ClientAuthentication
+import javax.inject.Inject
+
+internal class ActivityTraktAuthManager @Inject constructor(
+    @ApplicationContext private val context: Context,
+    private val traktManager: TraktManager,
+    private val requestProvider: Lazy<AuthorizationRequest>,
+    private val clientAuth: Lazy<ClientAuthentication>,
+) : TraktAuthManager {
+    private val authService by lazy(LazyThreadSafetyMode.NONE) {
+        AuthorizationService(context)
+    }
+
+    override fun buildLoginIntent(): Intent {
+        return authService.getAuthorizationRequestIntent(requestProvider.get())
+    }
+
+    override fun onLoginResult(result: LoginTrakt.Result) {
+        val (response, error) = result
+        when {
+            response != null -> {
+                authService.performTokenRequest(
+                    response.createTokenExchangeRequest(),
+                    clientAuth.get()
+                ) { tokenResponse, ex ->
+                    val state = AuthState().apply {
+                        update(tokenResponse, ex)
+                    }
+                    traktManager.onNewAuthState(state)
+                }
+            }
+            error != null -> logger.d(error, "AuthException")
+        }
+    }
+}
