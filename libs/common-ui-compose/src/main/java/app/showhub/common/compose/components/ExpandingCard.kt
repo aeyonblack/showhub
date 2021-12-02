8fee2d73f1effc49518177166aa1ec2614bd7d5f
aeyonblack <tanyamasvitaog@gmail.com>
2021-12-02 21:44:16 +0200

    modified action button background

--- libs/common-ui-compose/src/main/java/app/showhub/common/compose/components/ExpandingCard.kt
+++ libs/common-ui-compose/src/main/java/app/showhub/common/compose/components/ExpandingCard.kt
@@ -3,6 +3,7 @@ package app.showhub.common.compose.components
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -11,17 +12,22 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import app.showhub.common.compose.extensions.actionButtonBackground
 
 @Composable
 fun ExpandingCard(
     title: String,
     description: String,
     icon: Int,
-    subtitle: String,
     modifier: Modifier = Modifier,
-    shape: Shape = RoundedCornerShape(0.dp)
+    date: String? = null,
+    shape: Shape = RoundedCornerShape(0.dp),
+    watched: Boolean = false,
+    onWatchClicked: () -> Unit
 ) {
     val expanded by remember { mutableStateOf(false) }
 
@@ -44,16 +50,47 @@ fun ExpandingCard(
                 overflow = TextOverflow.Ellipsis
             )
             CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-                Row(
-                    modifier = Modifier.padding(top = 4.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        painter = painterResource(id = icon),
-                        contentDescription = null,
-                        modifier = Modifier.size(16.dp)
-                    )
+                if (date != null) {
+                    Row(
+                        modifier = Modifier.padding(top = 4.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            painter = painterResource(id = icon),
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp)
+                        )
+                        Text(
+                            text = date,
+                            style = MaterialTheme.typography.caption,
+                            modifier = Modifier.padding(start = 4.dp)
+                        )
+                    }
                 }
+            }
+            if (expanded) {
+                Text(
+                    text = description
+                )
+                Text(
+                    text = when {
+                        watched -> "Remove watch"
+                        else -> "Mark watched"
+                    },
+                    style = MaterialTheme.typography.h5,
+                    textAlign = TextAlign.Center,
+                    fontSize = 13.sp,
+                    color = MaterialTheme.colors.onBackground.copy(1f),
+                    modifier = Modifier
+                        .clickable {
+                            onWatchClicked()
+                        }
+                        .actionButtonBackground(
+                            enabled = !watched,
+                            alpha = 1f
+                        )
+                        .padding(horizontal = 8.dp, vertical = 4.dp)
+                )
             }
         }
     }
--- libs/common-ui-compose/src/main/java/app/showhub/common/compose/extensions/ActionButtonBackground.kt
+++ libs/common-ui-compose/src/main/java/app/showhub/common/compose/extensions/ActionButtonBackground.kt
@@ -4,26 +4,27 @@ import androidx.annotation.FloatRange
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
 
 fun Modifier.actionButtonBackground(
     enabled: Boolean = false,
     @FloatRange alpha: Float = 0.3f,
-    shape: Shape = RoundedCornerShape(6.dp)
+    shape: Shape = RoundedCornerShape(6.dp),
+    color: Color = Color.White
 ): Modifier = composed {
     if (enabled) {
         Modifier.background(
-            color = MaterialTheme.colors.onSurface.copy(alpha = alpha),
+            color = color.copy(alpha),
             shape = shape
         )
     } else {
         Modifier.border(
             width = 1.dp,
-            color = MaterialTheme.colors.onSurface.copy(alpha = alpha),
+            color = color.copy(alpha),
             shape = shape
         )
     }
