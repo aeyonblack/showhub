3382c286e5ae2a2f0a48b0eb59ff85cf0c121d33
aeyonblack <tanyamasvitaog@gmail.com>
2021-12-02 21:31:35 +0200

    added composition local provider of date and time formatter

--- libs/common-ui-compose/src/main/java/app/showhub/common/compose/utils/ShowhubCompositionLocalProvider.kt
+++ libs/common-ui-compose/src/main/java/app/showhub/common/compose/utils/ShowhubCompositionLocalProvider.kt
@@ -1,2 +1,8 @@
 package app.showhub.common.compose.utils
 
+import androidx.compose.runtime.staticCompositionLocalOf
+import com.tanya.base.android.appinitializer.util.ShowhubDateFormatter
+
+val LocalShowhubDateTimeFormatter = staticCompositionLocalOf<ShowhubDateFormatter> {
+    error("ShowhubDateFormatter not provided")
+}
