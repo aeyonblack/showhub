bd18455aab9e9caa927ce6c9de75adde0783cc51
aeyonblack <tanyamasvitaog@gmail.com>
2021-12-21 19:19:29 +0200

    added show data source interface

---
+++ libs/data/src/main/java/com/tanya/data/repositories/search/SearchDataSource.kt
@@ -0,0 +1,8 @@
+package com.tanya.data.repositories.search
+
+import com.tanya.data.entities.ShowEntity
+import com.tanya.data.entities.ShowImagesEntity
+
+interface SearchDataSource {
+    suspend fun search(query: String): Result<List<Pair<ShowEntity, List<ShowImagesEntity>>>>
+}
