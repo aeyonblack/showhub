b8570b559d14414c6cb7c9374929ced7ffacd12f
aeyonblack <tanyamasvitaog@gmail.com>
2021-12-21 19:58:33 +0200

    added tmdb search data source

--- libs/data/src/main/java/com/tanya/data/repositories/search/SearchDataSource.kt
+++ libs/data/src/main/java/com/tanya/data/repositories/search/SearchDataSource.kt
@@ -1,5 +1,6 @@
 package com.tanya.data.repositories.search
 
+import com.tanya.base.data.entities.Result
 import com.tanya.data.entities.ShowEntity
 import com.tanya.data.entities.ShowImagesEntity
 
--- libs/data/src/main/java/com/tanya/data/repositories/search/TmdbSearchDataSource.kt
+++ libs/data/src/main/java/com/tanya/data/repositories/search/TmdbSearchDataSource.kt
@@ -1,14 +1,23 @@
 package com.tanya.data.repositories.search
 
-import com.tanya.data.entities.ShowEntity
-import com.tanya.data.entities.ShowImagesEntity
+import com.tanya.base.data.entities.ErrorResult
+import com.tanya.base.extensions.executeWithRetry
+import com.tanya.base.extensions.toResult
+import com.tanya.data.mappers.TmdbShowResultsPageToShows
 import com.uwetrottmann.tmdb2.Tmdb
 import javax.inject.Inject
 
 class TmdbSearchDataSource @Inject constructor(
     private val tmdb: Tmdb,
+    private val mapper: TmdbShowResultsPageToShows
 ) : SearchDataSource {
-    override suspend fun search(query: String): Result<List<Pair<ShowEntity, List<ShowImagesEntity>>>> {
-
+    override suspend fun search(query: String) = try {
+        tmdb
+            .searchService()
+            .tv(query, 1, null, null, false)
+            .executeWithRetry()
+            .toResult(mapper::map)
+    } catch (t: Throwable) {
+        ErrorResult(t)
     }
 }
