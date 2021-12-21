9bfeacdb2dbc5e3f2d6f58f4425fabaf59aa3a28
aeyonblack <tanyamasvitaog@gmail.com>
2021-12-21 20:49:16 +0200

    finished setting up search view state

--- features/ui-search/src/main/java/com/tanya/ui/search/SearchViewState.kt
+++ features/ui-search/src/main/java/com/tanya/ui/search/SearchViewState.kt
@@ -1,5 +1,13 @@
 package com.tanya.ui.search
 
+import com.tanya.data.results.ShowDetailed
+
 internal data class SearchViewState(
     val query: String = "",
-)
+    val searchResults: List<ShowDetailed> = emptyList(),
+    val refreshing: Boolean = false
+) {
+    companion object {
+        val Empty = SearchViewState()
+    }
+}
