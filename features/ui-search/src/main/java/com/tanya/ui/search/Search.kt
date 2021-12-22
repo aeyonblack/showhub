5c01aa2a6f7cfd6f1fe8635e8a378ff49db5f1a0
aeyonblack <tanyamasvitaog@gmail.com>
2021-12-22 16:19:02 +0200

    skeleton for a search view

--- features/ui-search/src/main/java/com/tanya/ui/search/Search.kt
+++ features/ui-search/src/main/java/com/tanya/ui/search/Search.kt
@@ -1,2 +1,40 @@
 package com.tanya.ui.search
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import app.showhub.common.compose.utils.rememberFlowWithLifeCycle
+
+@Composable
+fun Search(
+    openShowDetails: (showId: Long) -> Unit
+) {
+    Search(
+        viewModel = hiltViewModel(),
+        openShowDetails = openShowDetails
+    )
+}
+
+@Composable
+internal fun Search(
+    viewModel: SearchViewModel,
+    openShowDetails: (showId: Long) -> Unit
+) {
+    val viewState by rememberFlowWithLifeCycle(flow = viewModel.state)
+        .collectAsState(initial = SearchViewState.Empty)
+    Search(viewState = viewState) {
+        when(it) {
+            is SearchAction.OpenShowDetails -> openShowDetails(it.showId)
+            else -> viewModel.submitAction(it)
+        }
+    }
+}
+
+@Composable
+internal fun Search(
+    viewState: SearchViewState,
+    dispatcher: (SearchAction) -> Unit
+) {
+
+}
