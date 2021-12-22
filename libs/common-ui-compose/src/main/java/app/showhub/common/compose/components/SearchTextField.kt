628251cc00c7a16748b6076bf51092547baf98be
aeyonblack <tanyamasvitaog@gmail.com>
2021-12-22 16:48:54 +0200

    added search text field

--- libs/common-ui-compose/src/main/java/app/showhub/common/compose/components/SearchTextField.kt
+++ libs/common-ui-compose/src/main/java/app/showhub/common/compose/components/SearchTextField.kt
@@ -1,10 +1,23 @@
 package app.showhub.common.compose.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Clear
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.TextFieldValue
+import com.tanya.common.ui.resources.R.drawable
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun SearchTextField(
     value: TextFieldValue,
@@ -15,5 +28,31 @@ fun SearchTextField(
     TextField(
         value = value,
         onValueChange = onValueChange,
+        leadingIcon = {
+            Icon(
+                painter = painterResource(id = drawable.ic_search),
+                contentDescription = null,
+                tint = Color.Black
+            )
+        },
+        trailingIcon = {
+            AnimatedVisibility(
+                visible = value.text.isNotEmpty(),
+                enter = fadeIn(),
+                exit = fadeOut()
+            ) {
+                IconButton(onClick = { onValueChange(TextFieldValue()) }) {
+                    Icon(
+                        imageVector = Icons.Rounded.Clear,
+                        contentDescription = null,
+                        tint = Color.Black
+                    )
+                }
+            }
+        },
+        placeholder = { Text(text = hint) },
+        maxLines = 1,
+        singleLine = true,
+        modifier = modifier
     )
 }
