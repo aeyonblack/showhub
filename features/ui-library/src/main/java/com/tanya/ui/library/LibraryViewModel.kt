2835bcd96f515f1ab03b3c2609fcfb61b77f67fa
aeyonblack <tanyamasvitaog@gmail.com>
2021-12-23 20:53:03 +0200

    created simple skeleton for view mode

--- features/ui-library/src/main/java/com/tanya/ui/library/LibraryViewModel.kt
+++ features/ui-library/src/main/java/com/tanya/ui/library/LibraryViewModel.kt
@@ -1,15 +1,23 @@
 package com.tanya.ui.library
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.tanya.common.ui.view.util.ObservableLoadingCounter
+import com.tanya.common.ui.view.util.ShowStateSelector
+import com.tanya.data.entities.SortOption
+import com.tanya.data.results.FollowedShowEntryWithShow
 import com.tanya.domain.interactors.ChangeShowFollowStatus
 import com.tanya.domain.interactors.UpdateFollowedShows
 import com.tanya.domain.observers.ObservePagedFollowedShows
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
 import javax.inject.Inject
 
 @HiltViewModel
-class LibraryViewModel @Inject constructor(
+internal class LibraryViewModel @Inject constructor(
     private val updateFollowedShows: UpdateFollowedShows,
     private val observePagedFollowedShows: ObservePagedFollowedShows,
     private val changeShowFollowStatus: ChangeShowFollowStatus
@@ -17,8 +25,53 @@ class LibraryViewModel @Inject constructor(
 
     private val pendingActions = MutableSharedFlow<LibraryAction>()
 
+    private val loadingState = ObservableLoadingCounter()
+    private val showSelection = ShowStateSelector()
+
+    val pagedList: Flow<PagingData<FollowedShowEntryWithShow>> =
+        observePagedFollowedShows.flow.cachedIn(viewModelScope)
+
+    private val availableSorts = listOf(
+        SortOption.SUPER_SORT,
+        SortOption.LAST_WATCHED,
+        SortOption.ALPHABETICAL,
+        SortOption.DATE_ADDED
+    )
+
+    private val sort = MutableStateFlow(SortOption.SUPER_SORT)
+
+    val state: StateFlow<LibraryViewState> = combine(
+        loadingState.observable,
+        showSelection.observeSelectedShowIds(),
+        showSelection.observeIsSelectionOpen(),
+        sort
+    ) { loading, selectedShowIds, isSelectionOpen, sort ->
+        LibraryViewState(
+            isLoading = loading,
+            selectionOpen = isSelectionOpen,
+            selectedShowIds = selectedShowIds,
+            availableSorts = availableSorts,
+            sort = sort
+        )
+    }.stateIn(
+        scope = viewModelScope,
+        started = WhileSubscribed(5000),
+        initialValue = LibraryViewState.Empty
+    )
 
     init {
+
+    }
+
+    private fun updateDataSource() {
+
+    }
+
+    private fun setSort(sort: SortOption) {
+
+    }
+
+    fun submitAction(action: LibraryAction) {
 
     }
 }
