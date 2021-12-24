f14331afdc2a78ee6ed89f2d394925ef0f49e33b
aeyonblack <tanyamasvitaog@gmail.com>
2021-12-24 18:42:45 +0200

    added some components to library feature

--- features/ui-library/src/main/java/com/tanya/ui/library/Library.kt
+++ features/ui-library/src/main/java/com/tanya/ui/library/Library.kt
@@ -1,8 +1,44 @@
 package com.tanya.ui.library
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import app.showhub.common.compose.utils.rememberFlowWithLifeCycle
+import com.tanya.data.results.FollowedShowEntryWithShow
 
 @Composable
-fun Library() {
+fun Library(
+    openShowDetails: (showId: Long) -> Unit
+) {
+    Library(
+        viewModel = hiltViewModel(),
+        openShowDetails = openShowDetails
+    )
+}
+
+@Composable
+internal fun Library(
+    viewModel: LibraryViewModel,
+    openShowDetails: (showId: Long) -> Unit
+) {
+    val viewState by rememberFlowWithLifeCycle(flow = viewModel.state)
+        .collectAsState(initial = LibraryViewState.Empty)
+    Library(
+        state = viewState,
+        list = rememberFlowWithLifeCycle(flow = viewModel.pagedList).collectAsLazyPagingItems()
+    ) {
+
+    }
+}
+
+@Composable
+internal fun Library(
+    state: LibraryViewState,
+    list: LazyPagingItems<FollowedShowEntryWithShow>,
+    dispatcher: (LibraryAction) -> Unit
+) {
 
 }
