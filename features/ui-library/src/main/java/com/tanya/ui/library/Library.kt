7701deae1323c08a1eb08b6f93c388d078864839
aeyonblack <tanyamasvitaog@gmail.com>
2021-12-24 19:10:09 +0200

    added simple modal bottom sheet

--- features/ui-library/src/main/java/com/tanya/ui/library/Library.kt
+++ features/ui-library/src/main/java/com/tanya/ui/library/Library.kt
@@ -1,13 +1,21 @@
 package com.tanya.ui.library
 
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import app.showhub.common.compose.utils.rememberFlowWithLifeCycle
+import com.google.accompanist.insets.ui.Scaffold
 import com.tanya.data.results.FollowedShowEntryWithShow
+import kotlinx.coroutines.launch
 
 @Composable
 fun Library(
@@ -34,11 +42,42 @@ internal fun Library(
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 internal fun Library(
     state: LibraryViewState,
     list: LazyPagingItems<FollowedShowEntryWithShow>,
     dispatcher: (LibraryAction) -> Unit
 ) {
+    val scope = rememberCoroutineScope()
+    val sheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
 
+    BackHandler(
+        enabled = sheetState.currentValue == ModalBottomSheetValue.Expanded,
+        onBack = {
+            scope.launch {
+                sheetState.animateTo(ModalBottomSheetValue.Hidden)
+            }
+        }
+    )
+
+    ModalBottomSheetLayout(
+        sheetContent = {
+            Column() {
+                Text(text = "modal", style = MaterialTheme.typography.h4)
+                Text(text = "modal", style = MaterialTheme.typography.h4)
+                Text(text = "modal", style = MaterialTheme.typography.h4)
+                Text(text = "modal", style = MaterialTheme.typography.h4)
+                Text(text = "modal", style = MaterialTheme.typography.h4)
+            }
+        },
+        sheetState = sheetState,
+        sheetShape = RoundedCornerShape(2.dp)
+    ) {
+        Scaffold(
+            topBar = {}
+        ) {
+
+        }
+    }
 }
