29373ba7cfe957c8fc5418edf8599a51f37cdb55
aeyonblack <tanyamasvitaog@gmail.com>
2021-12-25 18:48:11 +0200

    hoisted main ui composable

--- app/src/main/java/com/tanya/showhub/MainActivity.kt
+++ app/src/main/java/com/tanya/showhub/MainActivity.kt
@@ -3,23 +3,17 @@ package com.tanya.showhub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.rememberNavController
 import app.showhub.common.compose.theme.ShowhubTheme
 import app.showhub.common.compose.utils.LocalShowhubDateTimeFormatter
 import com.google.accompanist.insets.ProvideWindowInsets
-import com.google.accompanist.insets.ui.Scaffold
 import com.tanya.base.android.appinitializer.util.ShowhubDateFormatter
-import com.tanya.showhub.ui.components.BottomNavBar
 import com.tanya.showhub.ui.components.currentScreenAsState
+import com.tanya.showhub.ui.home.Home
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -41,36 +35,19 @@ class MainActivity : ComponentActivity() {
             ) {
                 ProvideWindowInsets(consumeWindowInsets = false) {
                     ShowhubTheme {
-                        /*TODO(remember to move this somewhere else)*/
-                        Scaffold(
-                            bottomBar = {
-                                BottomNavBar(
-                                    selectedNavigation = selectedItem,
-                                    navController = navController,
-                                    onNavigationSelected = {
-                                        navController.navigate(it.route) {
-                                            launchSingleTop = true
-                                            restoreState = true
-                                            popUpTo(navController.graph.findStartDestination().id) {
-                                                saveState = true
-                                            }
-                                        }
-                                    },
-                                )
-                            },
-                            backgroundColor = MaterialTheme.colors.background
-                        ) {
-                            Row(
-                                modifier = Modifier.fillMaxSize()
-                            ) {
-                                AppNavigation(
-                                    navHostController = navController,
-                                    modifier = Modifier
-                                        .weight(1f)
-                                        .fillMaxHeight()
-                                )
+                        Home(
+                            navController = navController,
+                            selectedNavigation = selectedItem,
+                            onNavigationSelected = {
+                                navController.navigate(it.route) {
+                                    launchSingleTop = true
+                                    restoreState = true
+                                    popUpTo(navController.graph.findStartDestination().id) {
+                                        saveState = true
+                                    }
+                                }
                             }
-                        }
+                        )
                     }
                 }
             }
--- app/src/main/java/com/tanya/showhub/ui/home/Home.kt
+++ app/src/main/java/com/tanya/showhub/ui/home/Home.kt
@@ -1,18 +1,44 @@
 package com.tanya.showhub.ui.home
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Scaffold
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import app.showhub.common.compose.components.AppBar
-
+import com.google.accompanist.insets.ui.Scaffold
+import com.tanya.showhub.AppNavigation
+import com.tanya.showhub.Screen
+import com.tanya.showhub.ui.components.BottomNavBar
 
 @Composable
-internal fun Library() {
+internal fun Home(
+    navController: NavHostController,
+    selectedNavigation: Screen,
+    onNavigationSelected: (Screen) -> Unit
+) {
     Scaffold(
-        topBar = { AppBar("Your Library")},
-        modifier = Modifier.fillMaxSize()
+        bottomBar = {
+            BottomNavBar(
+                selectedNavigation = selectedNavigation,
+                navController = navController,
+                onNavigationSelected = onNavigationSelected
+            )
+        },
+        backgroundColor = MaterialTheme.colors.background
     ) {
+        Row(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            AppNavigation(
+                navHostController = navController,
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+            )
+        }
     }
 }
 
