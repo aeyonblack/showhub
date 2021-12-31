7c98a4c12ebbb86e9caed0644bb279f3ab63d617
aeyonblack <tanyamasvitaog@gmail.com>
2021-12-31 21:26:10 +0200

    added view model for trending shows

--- features/ui-trending/src/main/java/com/tanya/ui/trending/TrendingShowsViewModel.kt
+++ features/ui-trending/src/main/java/com/tanya/ui/trending/TrendingShowsViewModel.kt
@@ -1,7 +1,29 @@
 package com.tanya.ui.trending
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
+import com.tanya.domain.observers.ObservePagedTrendingShows
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class TrendingShowsViewModel {
+class TrendingShowsViewModel @Inject constructor(
+    observePagedTrendingShows: ObservePagedTrendingShows
+) : ViewModel() {
+
+    val pagedList = observePagedTrendingShows.flow.cachedIn(viewModelScope)
+
+    init {
+        observePagedTrendingShows(ObservePagedTrendingShows.Params(PAGING_CONFIG))
+    }
+
+    companion object {
+        val PAGING_CONFIG = PagingConfig(
+            pageSize = 60,
+            initialLoadSize = 60
+        )
+    }
+
 }
