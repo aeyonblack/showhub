4f883f67ce38ce7caef53667c50617f072ca5bd8
aeyonblack <tanyamasvitaog@gmail.com>
2021-12-31 17:46:53 +0200

    added a view model for the popular shows screen

--- features/ui-popular/src/main/java/com/tanya/ui/popular/PopularShowsViewModel.kt
+++ features/ui-popular/src/main/java/com/tanya/ui/popular/PopularShowsViewModel.kt
@@ -1,4 +1,27 @@
 package com.tanya.ui.popular
 
-class PopularShowsViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
+import com.tanya.domain.observers.ObservePagedPopularShows
+import javax.inject.Inject
+
+class PopularShowsViewModel @Inject constructor(
+    observePagedPopularShows: ObservePagedPopularShows
+) : ViewModel() {
+
+    val pagedList = observePagedPopularShows.flow.cachedIn(viewModelScope)
+
+    init {
+        observePagedPopularShows(ObservePagedPopularShows.Params(PAGING_CONFIG))
+    }
+
+    companion object {
+        val PAGING_CONFIG = PagingConfig(
+            pageSize = 60,
+            initialLoadSize = 60
+        )
+    }
+
 }
