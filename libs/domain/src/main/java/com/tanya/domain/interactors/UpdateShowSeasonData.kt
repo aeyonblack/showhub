b6756397a1280b2b824db467089ae7dd4b27ad00
aeyonblack <tanyamasvitaog@gmail.com>
2022-01-04 16:42:27 +0200

    updated season data interactor

--- libs/domain/src/main/java/com/tanya/domain/interactors/UpdateShowSeasonData.kt
+++ libs/domain/src/main/java/com/tanya/domain/interactors/UpdateShowSeasonData.kt
@@ -1,20 +1,21 @@
 package com.tanya.domain.interactors
 
+import com.tanya.base.util.AppCoroutineDispatchers
 import com.tanya.data.repositories.episodes.SeasonsEpisodesRepository
 import com.tanya.data.repositories.followedshows.FollowedShowsRepository
 import com.tanya.domain.Interactor
 import com.tanya.domain.interactors.UpdateShowSeasonData.Params
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class UpdateShowSeasonData @Inject constructor(
     private val seasonsEpisodesRepository: SeasonsEpisodesRepository,
-    private val followedShowsRepository: FollowedShowsRepository
+    private val followedShowsRepository: FollowedShowsRepository,
+    private val dispatchers: AppCoroutineDispatchers
 ) : Interactor<Params>() {
 
-    override suspend fun doWork(params: Params) = withContext(Dispatchers.IO) {
+    override suspend fun doWork(params: Params) = withContext(dispatchers.io) {
         if (followedShowsRepository.isShowFollowed(params.showId)) {
             if (params.forceRefresh /*|| seasonsEpisodesRepository.needShowSeasonsUpdate(params.showId)*/) {
                 seasonsEpisodesRepository.updateSeasonsEpisodes(params.showId)
