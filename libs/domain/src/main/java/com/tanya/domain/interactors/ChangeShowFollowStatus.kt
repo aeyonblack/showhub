3235fa8112559a098bca1e837126bd9522af02d6
aeyonblack <tanyamasvitaog@gmail.com>
2022-01-04 16:33:58 +0200

    updated change show follow status

--- libs/domain/src/main/java/com/tanya/domain/interactors/ChangeShowFollowStatus.kt
+++ libs/domain/src/main/java/com/tanya/domain/interactors/ChangeShowFollowStatus.kt
@@ -1,6 +1,7 @@
 package com.tanya.domain.interactors
 
 import android.util.Log
+import com.tanya.base.util.AppCoroutineDispatchers
 import com.tanya.data.android.repository.images.ShowImagesStore
 import com.tanya.data.android.repository.shows.ShowsStore
 import com.tanya.data.extensions.fetch
@@ -9,7 +10,6 @@ import com.tanya.data.repositories.followedshows.FollowedShowsRepository
 import com.tanya.domain.Interactor
 import com.tanya.domain.interactors.ChangeShowFollowStatus.Action.*
 import com.tanya.domain.interactors.ChangeShowFollowStatus.Params
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -17,10 +17,11 @@ class ChangeShowFollowStatus @Inject constructor(
     private val followedShowsRepository: FollowedShowsRepository,
     private val seasonsEpisodesRepository: SeasonsEpisodesRepository,
     private val showsStore: ShowsStore,
-    private val showImagesStore: ShowImagesStore
+    private val showImagesStore: ShowImagesStore,
+    private val dispatchers: AppCoroutineDispatchers
 ) : Interactor<Params>() {
 
-     override suspend fun doWork(params: Params) = withContext(Dispatchers.IO) {
+     override suspend fun doWork(params: Params) = withContext(dispatchers.io) {
         params.showIds.forEach {
             when (params.action) {
                 TOGGLE ->
@@ -44,14 +45,11 @@ class ChangeShowFollowStatus @Inject constructor(
     }
 
     private suspend fun follow(showId: Long, deferDataFetch: Boolean) {
-        Log.d("changeShowFollow", "[Sof] - follow show")
         followedShowsRepository.addFollowedShow(showId)
-        Log.d("changeShowFollow", "[Mof] - follow show")
         if (!deferDataFetch) {
             seasonsEpisodesRepository.updateSeasonsEpisodes(showId)
             seasonsEpisodesRepository.updateShowEpisodeWatches(showId, forceRefresh = true)
         }
-        Log.d("changeShowFollow", "[Eof] - follow show")
     }
 
     private suspend fun unfollow(showId: Long) {
