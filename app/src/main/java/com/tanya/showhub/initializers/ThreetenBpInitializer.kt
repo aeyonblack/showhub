627979cfb2151dd1de5e271f275ea1d47487f242
aeyonblack <tanyamasvitaog@gmail.com>
2022-01-04 16:29:29 +0200

    inject dispatchers into threeten initializer

--- app/src/main/java/com/tanya/showhub/initializers/ThreetenBpInitializer.kt
+++ app/src/main/java/com/tanya/showhub/initializers/ThreetenBpInitializer.kt
@@ -3,19 +3,21 @@ package com.tanya.showhub.initializers
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
 import com.tanya.base.android.appinitializer.AppInitializer
+import com.tanya.base.util.AppCoroutineDispatchers
 import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.threeten.bp.zone.ZoneRulesProvider
 import javax.inject.Inject
 
-class ThreetenBpInitializer @Inject constructor() : AppInitializer {
+class ThreetenBpInitializer @Inject constructor(
+    private val dispatchers: AppCoroutineDispatchers
+) : AppInitializer {
 
     @OptIn(DelicateCoroutinesApi::class)
     override fun init(application: Application) {
         AndroidThreeTen.init(application)
-        GlobalScope.launch(Dispatchers.IO) {
+        GlobalScope.launch(dispatchers.io) {
             ZoneRulesProvider.getAvailableZoneIds()
         }
     }
