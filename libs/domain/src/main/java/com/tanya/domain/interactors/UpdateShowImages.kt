e8609f24782cd53eeffc675b09debce89f099ead
aeyonblack <tanyamasvitaog@gmail.com>
2022-01-04 16:41:35 +0200

    updated show images interactor

--- libs/domain/src/main/java/com/tanya/domain/interactors/UpdateShowImages.kt
+++ libs/domain/src/main/java/com/tanya/domain/interactors/UpdateShowImages.kt
@@ -1,18 +1,19 @@
 package com.tanya.domain.interactors
 
+import com.tanya.base.util.AppCoroutineDispatchers
 import com.tanya.data.android.repository.images.ShowImagesStore
 import com.tanya.data.extensions.fetch
 import com.tanya.domain.Interactor
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class UpdateShowImages @Inject constructor(
-    private val showImagesStore: ShowImagesStore
+    private val showImagesStore: ShowImagesStore,
+    private val dispatchers: AppCoroutineDispatchers
 ): Interactor<UpdateShowImages.Params>() {
 
     override suspend fun doWork(params: Params) {
-        withContext(Dispatchers.IO) {
+        withContext(dispatchers.io) {
             showImagesStore.fetch(params.showId, params.forceLoad)
         }
     }
