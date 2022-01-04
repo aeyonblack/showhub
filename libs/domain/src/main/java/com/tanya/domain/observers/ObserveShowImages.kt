2b90c852215ea2cd5ef780464e309c94b98650ba
aeyonblack <tanyamasvitaog@gmail.com>
2022-01-04 16:45:36 +0200

    updated show images observer

--- libs/domain/src/main/java/com/tanya/domain/observers/ObserveShowImages.kt
+++ libs/domain/src/main/java/com/tanya/domain/observers/ObserveShowImages.kt
@@ -1,25 +1,26 @@
 package com.tanya.domain.observers
 
 import com.dropbox.android.external.store4.StoreRequest
+import com.tanya.base.util.AppCoroutineDispatchers
 import com.tanya.data.android.repository.images.ShowImagesStore
 import com.tanya.data.entities.ShowImages
 import com.tanya.data.extensions.filterForResult
 import com.tanya.domain.SubjectInteractor
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class ObserveShowImages @Inject constructor(
-    private val store: ShowImagesStore
+    private val store: ShowImagesStore,
+    private val dispatchers: AppCoroutineDispatchers
 ): SubjectInteractor<ObserveShowImages.Params, ShowImages>() {
 
     override fun createObservable(params: Params): Flow<ShowImages> =
         store.stream(StoreRequest.cached(params.showId, refresh = false))
             .filterForResult()
             .map { ShowImages(it.requireData()) }
-            .flowOn(Dispatchers.Default)
+            .flowOn(dispatchers.computation)
 
     data class Params(val showId: Long)
 }
