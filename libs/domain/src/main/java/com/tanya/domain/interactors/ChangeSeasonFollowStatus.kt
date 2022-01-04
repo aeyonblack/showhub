85c94342cdbc8a12e8ab03b89b250c1931871e62
aeyonblack <tanyamasvitaog@gmail.com>
2022-01-04 16:31:38 +0200

    update change season follow interactor

--- libs/domain/src/main/java/com/tanya/domain/interactors/ChangeSeasonFollowStatus.kt
+++ libs/domain/src/main/java/com/tanya/domain/interactors/ChangeSeasonFollowStatus.kt
@@ -1,18 +1,19 @@
 package com.tanya.domain.interactors
 
+import com.tanya.base.util.AppCoroutineDispatchers
 import com.tanya.data.repositories.episodes.SeasonsEpisodesRepository
 import com.tanya.domain.Interactor
 import com.tanya.domain.interactors.ChangeSeasonFollowStatus.Action.*
 import com.tanya.domain.interactors.ChangeSeasonFollowStatus.Params
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class ChangeSeasonFollowStatus @Inject constructor(
-    private val repository: SeasonsEpisodesRepository
+    private val repository: SeasonsEpisodesRepository,
+    private val dispatchers: AppCoroutineDispatchers
 ): Interactor<Params>() {
 
-    override suspend fun doWork(params: Params) = withContext(Dispatchers.IO) {
+    override suspend fun doWork(params: Params) = withContext(dispatchers.io) {
         when (params.action) {
             FOLLOW -> repository.markSeasonFollowed(params.seasonId)
             IGNORE -> repository.markSeasonIgnored(params.seasonId)
