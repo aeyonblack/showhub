6300f35768e0cae13094887a40e044317fef4c10
aeyonblack <tanyamasvitaog@gmail.com>
2022-01-04 16:40:34 +0200

    updated show details interactor

--- libs/domain/src/main/java/com/tanya/domain/interactors/UpdateShowDetails.kt
+++ libs/domain/src/main/java/com/tanya/domain/interactors/UpdateShowDetails.kt
@@ -1,19 +1,20 @@
 package com.tanya.domain.interactors
 
+import com.tanya.base.util.AppCoroutineDispatchers
 import com.tanya.data.android.repository.shows.ShowsStore
 import com.tanya.data.extensions.fetch
 import com.tanya.domain.Interactor
-import com.tanya.domain.interactors.UpdateShowDetails.*
-import kotlinx.coroutines.Dispatchers
+import com.tanya.domain.interactors.UpdateShowDetails.Params
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class UpdateShowDetails @Inject constructor(
-    private val showsStore: ShowsStore
+    private val showsStore: ShowsStore,
+    private val dispatchers: AppCoroutineDispatchers
 ): Interactor<Params>() {
 
     override suspend fun doWork(params: Params) {
-        withContext(Dispatchers.IO) {
+        withContext(dispatchers.io) {
             showsStore.fetch(params.showId, params.forceLoad)
         }
     }
