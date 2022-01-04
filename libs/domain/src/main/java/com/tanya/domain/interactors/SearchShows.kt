1f75e2bb65dc5f46b198ca2827470297007ba1c1
aeyonblack <tanyamasvitaog@gmail.com>
2022-01-04 16:34:52 +0200

    updated search shows interactor

--- libs/domain/src/main/java/com/tanya/domain/interactors/SearchShows.kt
+++ libs/domain/src/main/java/com/tanya/domain/interactors/SearchShows.kt
@@ -1,22 +1,22 @@
 package com.tanya.domain.interactors
 
+import com.tanya.base.util.AppCoroutineDispatchers
 import com.tanya.data.daos.ShowFtsDao
 import com.tanya.data.repositories.search.SearchRepository
 import com.tanya.data.results.ShowDetailed
 import com.tanya.domain.SuspendingWorkInteractor
 import com.tanya.domain.interactors.SearchShows.Params
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class SearchShows @Inject constructor(
     private val searchRepository: SearchRepository,
     private val showFtsDao: ShowFtsDao,
-
+    private val dispatchers: AppCoroutineDispatchers
 ) : SuspendingWorkInteractor<Params, List<ShowDetailed>>() {
 
     override suspend fun doWork(params: Params) =
-        withContext(Dispatchers.IO) {
+        withContext(dispatchers.io) {
             val remoteResults = searchRepository.search(params.query)
             remoteResults.ifEmpty {
                 when {
