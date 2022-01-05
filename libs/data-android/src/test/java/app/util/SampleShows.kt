20021237209fc2fa46b017d852de735ccb0127a6
aeyonblack <tanyamasvitaog@gmail.com>
2022-01-05 20:05:14 +0200

    added mock functions for inserting and deleting a show

--- libs/data-android/src/test/java/app/util/SampleShows.kt
+++ libs/data-android/src/test/java/app/util/SampleShows.kt
@@ -1,5 +1,6 @@
 package app.util
 
+import com.tanya.data.ShowhubDatabase
 import com.tanya.data.entities.ShowEntity
 
 /*sample mock show ids*/
@@ -11,3 +12,7 @@ const val showId3 = 3L
 val show = ShowEntity(id = showId1, title = "The Theory of Everything", traktId = 133)
 val show2 = ShowEntity(id = showId2, title = "A Brief History of Time", traktId = 1293)
 val show3 = ShowEntity(id = showId3, title = "Lost in Space", traktId = 327)
+
+/*mock functions for deleting and inserting a show into the database*/
+internal suspend fun insertShow(db: ShowhubDatabase) = db.showDao().insert(show)
+internal suspend fun deleteShow(db: ShowhubDatabase) = db.showDao().delete(show)
