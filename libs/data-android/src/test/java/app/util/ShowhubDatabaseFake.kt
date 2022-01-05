cea6f8ae7eccdab61f97a40f9b4c800529df4321
aeyonblack <tanyamasvitaog@gmail.com>
2022-01-05 20:43:51 +0200

    created a showhub database fake

---
+++ libs/data-android/src/test/java/app/util/ShowhubDatabaseFake.kt
@@ -0,0 +1,35 @@
+package app.util
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.tanya.data.ShowhubDatabase
+import com.tanya.data.android.repository.database.ShowhubTypeConverters
+import com.tanya.data.entities.*
+import com.tanya.data.views.FollowedShowsLastWatched
+import com.tanya.data.views.FollowedShowsNextToWatch
+import com.tanya.data.views.FollowedShowsWatchStats
+
+@Database(
+    entities = [
+        ShowEntity::class,
+        ShowFts::class,
+        ShowImagesEntity::class,
+        PopularShowEntity::class,
+        TrendingShowEntity::class,
+        RelatedShowEntity::class,
+        FollowedShowEntity::class,
+        EpisodeEntity::class,
+        EpisodeWatchEntity::class,
+        SeasonEntity::class,
+        WatchedShowEntity::class
+    ],
+    views = [
+        FollowedShowsWatchStats::class,
+        FollowedShowsLastWatched::class,
+        FollowedShowsNextToWatch::class
+    ],
+    version = 1
+)
+@TypeConverters(ShowhubTypeConverters::class)
+abstract class ShowhubDatabaseFake: RoomDatabase(), ShowhubDatabase
