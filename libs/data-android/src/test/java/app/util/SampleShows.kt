0d66e53a1e0b7100bb2457bb9bd777515b601af9
aeyonblack <tanyamasvitaog@gmail.com>
2022-01-05 20:07:59 +0200

    added sample seasons ids

--- libs/data-android/src/test/java/app/util/SampleShows.kt
+++ libs/data-android/src/test/java/app/util/SampleShows.kt
@@ -4,15 +4,21 @@ import com.tanya.data.ShowhubDatabase
 import com.tanya.data.entities.ShowEntity
 
 /*sample mock show ids*/
-const val showId1 = 1L
+const val showId = 1L
 const val showId2 = 2L
 const val showId3 = 3L
 
 /*sample mock shows*/
-val show = ShowEntity(id = showId1, title = "The Theory of Everything", traktId = 133)
+val show = ShowEntity(id = showId, title = "The Theory of Everything", traktId = 133)
 val show2 = ShowEntity(id = showId2, title = "A Brief History of Time", traktId = 1293)
 val show3 = ShowEntity(id = showId3, title = "Lost in Space", traktId = 327)
 
 /*mock functions for deleting and inserting a show into the database*/
 internal suspend fun insertShow(db: ShowhubDatabase) = db.showDao().insert(show)
 internal suspend fun deleteShow(db: ShowhubDatabase) = db.showDao().delete(show)
+
+/*sample show season ids for show (1)*/
+const val s1_id = 1L
+const val s2_id = 2L
+const val s0_id = 3L
+
