88d7f22c62262b75abce24cfff90dcba70ef234f
aeyonblack <tanyamasvitaog@gmail.com>
2022-01-06 17:42:15 +0200

    test room database module

--- libs/data-android/src/test/java/app/di/TestShowhubDatabaseModule.kt
+++ libs/data-android/src/test/java/app/di/TestShowhubDatabaseModule.kt
@@ -1,11 +1,16 @@
 package app.di
 
+import android.content.Context
+import androidx.room.Room
+import app.util.FakeTransactionRunner
+import app.util.ShowhubDatabaseFake
 import com.tanya.base.util.Logger
 import com.uwetrottmann.tmdb2.Tmdb
 import com.uwetrottmann.trakt5.TraktV2
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import io.mockk.mockk
 import javax.inject.Singleton
@@ -23,5 +28,22 @@ object TestShowhubDatabaseModule {
     @Singleton
     @Provides
     fun provideLogger(): Logger = mockk(relaxUnitFun = true)
+
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestRoomDatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context) =
+        Room.inMemoryDatabaseBuilder(context, ShowhubDatabaseFake::class.java)
+            .allowMainThreadQueries()
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideDatabaseTransactionRunner() = FakeTransactionRunner()
 
 }
