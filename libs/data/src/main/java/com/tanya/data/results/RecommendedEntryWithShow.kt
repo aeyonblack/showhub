17b8ef2fc4c4f1a8fe7e325deac337d210ccc6b6
aeyonblack <tanyamasvitaog@gmail.com>
2022-01-31 17:14:31 +0200

    added a recommended show entry domain entity

---
+++ libs/data/src/main/java/com/tanya/data/results/RecommendedEntryWithShow.kt
@@ -0,0 +1,40 @@
+package com.tanya.data.results
+
+import androidx.room.Embedded
+import androidx.room.Ignore
+import androidx.room.Relation
+import com.tanya.data.entities.*
+import java.util.*
+
+class RecommendedEntryWithShow : EntryWithShow<RecommendedShowEntity> {
+
+    @Embedded
+    override lateinit var entry: RecommendedShowEntity
+
+    @Relation(parentColumn = "show_id", entityColumn = "id")
+    override lateinit var relations: List<ShowEntity>
+
+    @Relation(parentColumn = "show_id", entityColumn = "show_id")
+    override lateinit var images: List<ShowImagesEntity>
+
+    @delegate:Ignore
+    val backdrop by lazy(LazyThreadSafetyMode.NONE) {
+        images.findHighestRatedBackdrop()
+    }
+
+    @delegate:Ignore
+    override val poster by lazy(LazyThreadSafetyMode.NONE) {
+        images.findHighestRatedPoster()
+    }
+
+    override fun equals(other: Any?): Boolean = when {
+        other === this -> true
+        other is RecommendedEntryWithShow -> {
+            entry == other.entry && relations == other.relations && images == other.images
+        }
+        else -> false
+    }
+
+    override fun hashCode(): Int = Objects.hash(entry, relations, images)
+
+}
