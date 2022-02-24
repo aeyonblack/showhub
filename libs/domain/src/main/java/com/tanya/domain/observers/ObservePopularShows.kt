730b950fb476fdb65b30203db52a00825b7be747
aeyonblack <tanyamasvitaog@gmail.com>
2022-02-24 20:38:10 +0200

    renamed a badly named variable

--- libs/domain/src/main/java/com/tanya/domain/observers/ObservePopularShows.kt
+++ libs/domain/src/main/java/com/tanya/domain/observers/ObservePopularShows.kt
@@ -7,13 +7,13 @@ import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ObservePopularShows @Inject constructor(
-    private val popularShowsRepository: PopularDao
+    private val popularShowsDao: PopularDao
 ) : SubjectInteractor<ObservePopularShows.Params, List<PopularEntryWithShow>>() {
 
     override fun createObservable(
         params: Params
     ): Flow<List<PopularEntryWithShow>> =
-        popularShowsRepository.entriesObservable(params.count, 0)
+        popularShowsDao.entriesObservable(params.count, 0)
     
     data class Params(val count: Int  = 20)
 }
